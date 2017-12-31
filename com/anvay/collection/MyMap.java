6e6010b253376dcc06a94c9efa16a7307e3ef2a9
Anvay <anvay.kolhatkar@gmail.com>
2017-12-31 15:34:47 -0800

    Changing MyMap to add a constructor

--- com/anvay/collection/MyMap.java
+++ com/anvay/collection/MyMap.java
@@ -1,10 +1,18 @@
 package com.anvay.collection;
 
+import java.util.List;
 public class MyMap {
+	
+	List<Object> keys;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	}
+	
+	public MyMap() {
+		System.out.println("The MyMap is created");
+		
 	}
 
 }
